965c108d5b41b75fa65903dcc3fc36661aef0c98
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-25 22:29:34 +0200

    code: add create task function and add to urls

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import c_index, index
+from .views import c_index, create_task, index
 
 urlpatterns = [
     path("", index, name="index"),
     path("cotton/", c_index, name="c_index"),
+    path("create/", create_task, name="create_task"),
 ]
--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
+from .forms import TaskForm
+from .models import Task
+
 
 def index(request):
     return render(request, "index.html")
 
 
 def c_index(request):
-    return render(request, "c_index.html")
+    form = TaskForm()
+    return render(request, "c_index.html", {"tasks": Task.objects.all(), "form": form})
+
+
+def create_task(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        task = form.save(commit=False)
+        task.author = request.user
+        task.save()
+    return render(request, "components/task/list.html", {"data": Task.objects.all()})
