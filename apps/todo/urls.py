a28400cf9d2ab35316c1cfde6b657ae4019ba53b
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-25 23:25:41 +0200

    code: changed cotton url name to home

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -4,6 +4,6 @@ from .views import c_index, create_task, index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("cotton/", c_index, name="c_index"),
+    path("cotton/", c_index, name="home"),
     path("create/", create_task, name="create_task"),
 ]
