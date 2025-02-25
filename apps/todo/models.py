9f610f645b765deecc2c4b2409b45354be03708d
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-25 20:20:54 +0200

    code: added task model

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    class Categories(models.TextChoices):
+        WORK = "work", "Work"
+        Home = "home", "Home"
+        SCHOOL = "school", "School"
+
+    content = models.TextField()
+    categories = models.CharField(max_length=7, choices=Categories.choices, default=Categories.Home)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    created_at = models.DateTimeField(auto_now=True)
