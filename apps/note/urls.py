e0da1598b3c59b8aa0912d1781ab851b64787127
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-26 02:01:57 +0200

    chore: remove unused

--- apps/note/urls.py
+++ apps/note/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import c_index, create_note, delete_note, index
+from .views import create_note, delete_note, index
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("cotton/", c_index, name="home"),
+    path("", index, name="home"),
     path("create/", create_note, name="create_note"),
     path("delete/<int:id>", delete_note, name="delete_note"),
 ]
